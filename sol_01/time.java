ef8f5493cb29c20604bfe9d72616d4fa1b09191c
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-02-26 16:35:12 +0700

    Create time.java

---
+++ sol_01/time.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sol_01;
+
+/**
+ *
+ * @author Huy Toan
+ */
+public class time {
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    public time() {
+
+    }
+
+    public time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void settime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + hour + ":" + minute + ":" + second + "\"";
+    }
+}
