db78f9538fe1eaf1bb51d46b750938bfaeb78cb9
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-02-26 16:34:39 +0700

    Create circle.java

---
+++ sol_01/circle.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sol_01;
+
+/**
+ *
+ * @author Huy Toan
+ */
+public class circle {
+
+    private double radius;
+
+    public circle() {
+        radius = 1;              
+    }
+
+    public circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "circle{" + "radius=" + radius + '}';
+    }
+}
