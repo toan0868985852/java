a013d9b8b1f95da56b65ffdbb35d5e0077bfa2e6
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-03-03 13:01:49 +0700

    Create author.java

---
+++ jp_4/author.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp_4;
+
+/**
+ *
+ * @author Huy Toan
+ */
+public class author {
+    private String name;
+    private String email;
+    private char gender;
+    
+    public author(){
+        
+    }
+
+    public author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+        return "author{" + "name=" + name + ", email=" + email + ", gender=" + gender + '}';
+    }
+    
+    
+    
+}
