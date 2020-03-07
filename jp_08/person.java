027181ea037a6c808c4ce77483e4f1da9dfafd7e
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-03-07 13:03:24 +0700

    Create person.java

---
+++ jp_08/person.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp_08;
+
+/**
+ *
+ * @author Huy Toan
+ */
+public class person {
+    private String name;
+    private String address;
+
+    public person() {
+    }
+
+    public person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "person[" + "name=" + name + ", address=" + address + ']';
+    }
+    
+    
+}
