cf9ac8700e497fb2be429952de2efeeb37c50305
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-03-12 13:34:18 +0700

    Create GeometricObject.java

---
+++ jp_11/GeometricObject.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp_11;
+
+/**
+ *
+ * @author Huy Toan
+ */
+public interface GeometricObject {
+    public double getPerimete();
+    public double getArea();
+}
