bbeb42b7e899a84702eebaf75d66734d04898258
Nguyễn Huy Toản <54660134+toan0868985852@users.noreply.github.com>
2020-03-12 13:34:27 +0700

    Create Movable.java

---
+++ jp_11/Movable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp_11;
+
+/**
+ *
+ * @author Huy Toan
+ */
+public interface Movable {
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
